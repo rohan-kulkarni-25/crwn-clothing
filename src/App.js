641f0d63b053f3dd285869a0a65e8f69f90ce28f
Rohan Kulkarni <rohank2502@gmail.com>
2023-02-02 09:19:28 +0530

    router update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from "./routes/home/home.component";
 
-function App() {
+// React Routing
+import { Route, Routes } from 'react-router-dom'
+
+
+const Shop = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>HELLO</h1>
   );
+};
+
+const Navigation = () => {
+  return (
+    <div>
+      <div>
+        <h1>HOME</h1>
+      </div>
+      <div>
+        <h1>HOME</h1>
+      </div>
+      <div>
+        <h1>HOME</h1>
+      </div>
+      <div>
+        <h1>HOME</h1>
+      </div>
+    </div>
+  )
 }
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />} >
+        <Route path="/home" element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+      </Route>
+    </Routes >
+  );
+};
 
 export default App;
---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import CategoryItem from "../category-item/category-item.component";
+import "./directory.styles.scss";
+
+
+const Directory = ({ categories }) => {
+  return (
+    <div className="categories-container">
+      {categories.map((category) => {
+        return <CategoryItem category={category} />;
+      })}
+    </div>
+  )
+}
+
+export default Directory
