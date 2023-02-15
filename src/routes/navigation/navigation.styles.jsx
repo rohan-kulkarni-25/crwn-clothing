1a80e484b73937ac70411b7491ed7848adbf551b
Rohan Kulkarni <rohank2502@gmail.com>
2023-02-15 11:45:31 +0530

    updated nav link

--- src/routes/navigation/navigation.styles.jsx
+++ src/routes/navigation/navigation.styles.jsx
@@ -23,7 +23,7 @@ export const NavLinksContainer = styled.div`
   justify-content: flex-end;
 `;
 
-export const NavLink = styled.div`
+export const NavLink = styled(Link)`
   padding: 10px 15px;
   cursor: pointer;
 `;
